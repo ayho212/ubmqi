ef95285b04d7f2f91681873d2a97e45091271b13
ayho212 <almahdykarawang1@gmail.com>
2025-12-08 17:16:20 +0700

    Add Tailwind CSS configuration file
    Signed-off-by: ayho212 <almahdykarawang1@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0ea5a4",
+        accent: "#06b6d4"
+      }
+    }
+  },
+  plugins: []
+}
